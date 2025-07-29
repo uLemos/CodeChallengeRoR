e352f2c096e9b3e1e659c283043a511f7e60bfd3
uLemos <fsnvag12@gmail.com>
2025-07-29 15:20:18 -0300

    :memo: ajuste dos testes do readlater e adição de validação das propriedades que estão presentes no article

--- test/controllers/save_for_later_controller_test.rb
+++ test/controllers/save_for_later_controller_test.rb
@@ -5,16 +5,29 @@ class SaveForLaterControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @user = users(:one)
+    @article = articles(:one)
     sign_in @user
   end
 
   test "should create readlater" do
-    article = articles(:one)
-
-    ReadLater.where(article_id: article.id, user_id: @user.id).destroy_all
+    ReadLater.where(article_id: @article.id, user_id: @user.id).destroy_all
 
     assert_difference("ReadLater.count", 1) do
-      post save_for_later_path, params: { id: article.id }
+      post save_for_later_path, params: { id: @article.id }
+    end
+
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "should remove article from read later" do
+    post save_for_later_path, params: { id: @article.id }
+
+    read_later = @user.read_laters.find_by(article_id: @article.id)
+
+    assert_difference("ReadLater.count", -1) do
+      delete news_read_later_path(news_id: @article.id, id: read_later.id)
     end
 
     assert_response :redirect
