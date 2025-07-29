2c95e9fa36d9e6d4c10016c8de4681ac68115dd9
uLemos <fsnvag12@gmail.com>
2025-07-29 09:20:02 -0300

    :memo: Ajustando o teste SaveForLater de get para post e criando um usuário de autenticação

--- test/controllers/save_for_later_controller_test.rb
+++ test/controllers/save_for_later_controller_test.rb
@@ -1,8 +1,18 @@
 require "test_helper"
 
 class SaveForLaterControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get save_for_later_create_url
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
+  test "should create readlater" do
+    assert_difference("ReadLater.count", 1) do
+      post save_for_later_create_url, params: { article_id: article_id }
+    end
+
+    assert_response :redirect
+    follow_redirect!
     assert_response :success
   end
 end
