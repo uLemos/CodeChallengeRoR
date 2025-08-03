aa678756529515c25085735e247e07689f6455dc
uLemos <fsnvag12@gmail.com>
2025-08-03 17:28:33 -0300

    Teste Tailwind

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
+  
+  config.assets.compile = false
+  config.assets.digest = true
 
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
